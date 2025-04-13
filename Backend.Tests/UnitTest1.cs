b6a831fe9324088076a5b021e27df31ac73f7b59
JAS0N-SMITH <huchknows@gmail.com>
2025-04-13 19:40:00 -0400

    fix: fixed tests; adjusted ci-cd

---
+++ Backend.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Backend.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+
+    public class ExampleTests
+    {
+        [Fact]
+        public void ExampleTest_ShouldPass()
+        {
+            // Arrange
+            int expected = 5;
+            int actual = 2 + 3;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
